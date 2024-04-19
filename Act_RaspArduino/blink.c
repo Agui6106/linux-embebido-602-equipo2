4c4fa47cc07b1d95da3593a46012f45af5ce68f4
Agui6106 <A01735612@tec.mx>
2024-04-19 12:55:50 -0600

    Update files

--- Act_RaspArduino/blink.c
+++ Act_RaspArduino/blink.c
@@ -5,11 +5,23 @@
 int main (void)
 {
     DDRB |= _BV(PORTB0);      // set pin 0 of port B as an output pin
+    DDRB |= _BV(PORTB1);      // set pin 0 of port B as an output pin
+    DDRB |= _BV(PORTB2);      // set pin 0 of port B as an output pin
 
     for (;;) {
+        //PRENDE
         PORTB |= _BV(PORTB0);  // set pin 0 of port B high
-        _delay_ms(1000);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        _delay_ms(200);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        PORTB |= _BV(PORTB1);  // set pin 0 of port B high
+        _delay_ms(200);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        PORTB |= _BV(PORTB2);  // set pin 0 of port B high
+        _delay_ms(200);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        //APAGA
         PORTB &= ~_BV(PORTB0); // set pin 0 of port B low
-        _delay_ms(1000);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        _delay_ms(200);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        PORTB &= ~_BV(PORTB1); // set pin 0 of port B low
+        _delay_ms(200);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
+        PORTB &= ~_BV(PORTB2); // set pin 0 of port B low
+        _delay_ms(200);  // loop for 62500 iterations * 4 cycles / 1MHz clock ~= 250ms
     }
 }
