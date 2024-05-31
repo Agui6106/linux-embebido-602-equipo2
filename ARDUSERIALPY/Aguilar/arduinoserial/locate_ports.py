fc63a6871882fd05f7bcd915fc32ebf647845ccf
Agui6106 <A01735612@tec.mx>
2024-05-31 12:59:22 -0600

    Uploaded final app

--- ARDUSERIALPY/Aguilar/arduinoserial/locate_ports.py
+++ ARDUSERIALPY/Aguilar/arduinoserial/locate_ports.py
@@ -1,17 +1,16 @@
+## Encontrar los puertos seriales disponibles
 # -- Encontrar los puertos seriales disponibles -- #
-# Importamos 
 import serial.tools.list_ports
 
-# Declaramos la funcion
-def find_serial_ports():
+def find_ports() -> list[str]:
     ports = serial.tools.list_ports.comports()
     serial_ports = []
     for port in ports:
         serial_ports.append(port.device)
     return serial_ports
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     print("Puertos seriales disponibles:")
-    ports = find_serial_ports()
+    ports = find_ports()
     for port in ports:
         print(port)
